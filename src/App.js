ea0cd14396b607261a8f9603317733f53169e1ec
fatemekholousi <fatemekholousizaer@gmail.com>
2022-06-03 00:31:20 +0430

    prettier changes added

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
-import './app.scss';
-import InfoCard from './components/InfoCard';
-import SideBar from './components/SideBar';
-import SimCardInfo from './components/SimCardInfo';
-import "./assets/fonts/lale-zar/Lalezar-Regular.ttf"
-import "./assets/fonts/vazir/Vazir-Code.ttf"
-import HeaderCard from './components/HeaderCard';
-import SuggestPackages from './components/SuggestPackages';
+import './app.scss'
+import InfoCard from './components/InfoCard'
+import SideBar from './components/SideBar'
+import SimCardInfo from './components/SimCardInfo'
+import './assets/fonts/lale-zar/Lalezar-Regular.ttf'
+import './assets/fonts/vazir/Vazir-Code.ttf'
+import HeaderCard from './components/HeaderCard'
+import SuggestPackages from './components/SuggestPackages'
 
 function App() {
   return (
-    <div className="my-irancell-page" >
+    <div className="my-irancell-page">
+      <aside>
+        <InfoCard />
+        <SimCardInfo />
+      </aside>
 
-  
-   <aside>
-    <InfoCard/>
-    <SimCardInfo/>
-   </aside>
- 
-   <main>
-     <h1>
-       صفحه اصلی
-     </h1>
-<HeaderCard/>
-<SuggestPackages/>
-
-   </main>
-   <SideBar/>
-
+      <main>
+        <h1>صفحه اصلی</h1>
+        <HeaderCard />
+        <SuggestPackages />
+      </main>
+      <SideBar />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
