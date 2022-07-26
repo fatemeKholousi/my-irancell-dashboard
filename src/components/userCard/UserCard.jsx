4953b30b03b4af70e07c0262c44a84c1d40c61d9
fateme kholousi <fatemekholousizaer@gmail.com>
2022-07-26 18:04:05 +0430

    fix: profile name change

--- src/components/userCard/UserCard.jsx
+++ src/components/userCard/UserCard.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import TextCard from '../textCards/TextCard'
-import PersonImage from './tumblr_ps769izf231qzm1v9o2_r7_1280.jpg'
+import PersonImage from './profile.jpg'
 import "./userCard.css"
 
 function UserCard() {
   return (
     <div className='user-card'>
-        <TextCard userCard={true} text1='سلام، غزاله سلام پارمیدا' text2='+98 0919191991' icon={<img src={PersonImage} width="60" />} />
+        <TextCard userCard={true} text1='سلام، فاطمه' text2='+98 0919191991' icon={<img src={PersonImage} width="60" />} />
     </div>
   )
 }
